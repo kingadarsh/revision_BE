3d5203b7cf2deb6ea60c17e781fdedb4f45d07a1
kingadarsh <adarsh261201@gmail.com>
2025-01-25 15:20:35 +0530

    Added a command

--- CLI_COMMANDER_FILESYSTEM/index.js
+++ CLI_COMMANDER_FILESYSTEM/index.js
@@ -11,9 +11,12 @@ const program=new Command();
 //         console.log("File content: ",data);
 //     }
 // });
+program.name("counter")
+.description("ClI tool to count words/sentences in a file")
+.version('0.8.0');
 
 
-program.name("Count-Word")
+program.command("Count-Word")
 .description("A CLI tool to count words in a file")
 .argument("<file>","name of the file from where to count the words")
 .action((file)=>{
@@ -33,5 +36,26 @@ program.name("Count-Word")
         }
     })
 });
+
+program.command("Count-Sentences")
+.description("Command to count sentences in a file ")
+.arguments("<file>","Name of the file from where to count sentences")
+.action((file)=>{
+    fs.readFile(file,"utf8",(err,data)=>{
+        if(err){
+            return console.error("There was a error :",err.message);
+        }
+        else{
+            let z=0;
+            for(let i=0;i<data.length;i++){
+                if(data[i]=="?" || data[i]=="!" || data[i]=="." || data[i]==";"){
+                    z++;
+                }
+            }
+            console.log(`The total sentences in the file is ${z}`);
+        }
+    })
+})
+
 
 program.parse();
